0b10fd7a1f30a7e412581f9fe9f39188bd197809
Adam Hopkins <adam@optymizer.com>
2017-02-22 10:37:16 +0200

    v. 0.2.5.1

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 setup(
   name = 'elasticmanager',
-  packages = ['elasticmanager','elasticmanager.management'],
-  version = '0.2.5',
+  packages = ['elasticmanager','elasticmanager.management','elasticmanager.management.commands'],
+  version = '0.2.5.1',
   description = 'Django model manager like class for elasticsearch integration',
   author = 'Adam Hopkins',
   author_email = 'adam@optymizer.com',
