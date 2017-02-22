30659572381c9a01f7cf6990da119af39d02a1d1
Adam Hopkins <adam@optymizer.com>
2017-02-22 10:33:38 +0200

    v.0.2.5 - fix missing management folder

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
   name = 'elasticmanager',
-  packages = ['elasticmanager'],
-  version = '0.2.4',
+  packages = ['elasticmanager','elasticmanager.management'],
+  version = '0.2.5',
   description = 'Django model manager like class for elasticsearch integration',
   author = 'Adam Hopkins',
   author_email = 'adam@optymizer.com',
   url = 'https://github.com/ahopkins/elasticmanager',
   download_url = 'https://github.com/ahopkins/elasticmanager/archive/master.zip',
-  keywords = ['django', 'elasticsearch'],
+  keywords = ['django', 'elasticsearch',],
   install_requires=[
       'elasticsearch_dsl',
   ],
